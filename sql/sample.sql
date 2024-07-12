808f14bcb8d91ef8970b7f0fce678d7ece0b64f5
Abraham <35835266+towett254@users.noreply.github.com>
2024-07-12 13:28:30 +0300

    Debug connection

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,9 +1,59 @@
-CREATE DATABASE your_database;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 12, 2024 at 12:25 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
-USE your_database;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE your_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    about INT NOT NULL
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `csv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `your_table`
+--
+
+CREATE TABLE `your_table` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `about` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `your_table`
+--
+ALTER TABLE `your_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `your_table`
+--
+ALTER TABLE `your_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
